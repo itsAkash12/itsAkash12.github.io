fd920af2b28d52122ed98486e4012b42771873d7
itsAkash12 <akashviratsingh@gmail.com>
2022-10-28 20:38:54 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import About from './Components/about/About';
+import AboutMe from './Components/aboutMe/AboutMe';
 import Header from './Components/header/Header';
+import Projects from './Components/projects/Projects';
+import Skills from './Components/skills/Skills';
 function App() {
   return (
     <div className="App">
       <Header/>
       <About />
+      <AboutMe />
+      <Skills /> 
+      <Projects />
     </div>
   );
 }
--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./About.css";
 import profile from "../images/final_profile_pic.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import AOS from "aos"
 import Typewriter from "typewriter-effect";
 export default function About() {
+  useEffect(() => {
+    AOS.init({
+      duration : 2000
+    });
+  }, []);
   return (
     <div className="about">
       <div className="about-container">
-        <div className="left">
+        <div className="left" data-aos={"fade-left"}>
           <p className="hello">Hello 👋.</p>
           <p className="intro">
-            I'm<span id="name"> Akash Kumar Singh</span>
+            I'm<span id="name"><Typewriter
+              options={{
+                strings: [
+                  "Akash Kumar Singh",
+                  "आकाश कुमार सिंह",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            /></span>
           </p>
           <div className="type">
             <Typewriter
